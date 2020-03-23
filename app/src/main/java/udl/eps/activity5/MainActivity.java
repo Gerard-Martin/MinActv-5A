aad7eb9e322faf622ceaae8ce9084bcb9d853ea2
Gerard-Martin <gml3@alumnes.udl.cat>
2020-03-23 12:41:46 +0100

    Todo en AsyncTask

--- app/src/main/java/udl/eps/activity5/MainActivity.java
+++ app/src/main/java/udl/eps/activity5/MainActivity.java
@@ -1,7 +1,10 @@
 package udl.eps.activity5;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView1;
     private TextView textView2;
 
+    boolean wifiConnected;
+    boolean mobileConnected;
+
     NetworkInfo networkInfo;
 
     @Override
@@ -27,32 +33,25 @@ public class MainActivity extends AppCompatActivity {
         textView1.setBackgroundColor(Color.YELLOW);
         textView2 = findViewById(R.id.textView2);
         textView2.setBackgroundColor(Color.GREEN);
-
+        BroadcastReceiver br = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        this.registerReceiver(br, filter);
         checkConnectivity();
     }
 
     private void checkConnectivity() {
-        ConnectivityManager connMgr =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        networkInfo = connMgr.getActiveNetworkInfo();
-        String base = " Information:\n \tType: %s \n \tAvailable: %s\n \tConnected: %s\n \tRoaming: %s\n";
-        String result = "";
-        if(networkInfo != null) {
-            result = String.format(base, networkInfo.getType(), networkInfo.isAvailable(), networkInfo.isConnected(), networkInfo.isRoaming());
-        }else{
-            result = "There is no network connected!\n";
-            result += String.format(base, "-", "-", "-", "-");
-        }
-        textView1.setText(result);
         new NetworkAsyncTask().execute();
+
     }
 
     private class NetworkAsyncTask extends AsyncTask<String, Void, String> {
+        String result = "";
 
         @Override
         protected String doInBackground(String... strings) {
-            boolean wifiConnected;
-            boolean mobileConnected;
+            ConnectivityManager connMgr =
+                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            networkInfo = connMgr.getActiveNetworkInfo();
             if (networkInfo != null && networkInfo.isConnected()) {
                 wifiConnected = networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
                 mobileConnected = networkInfo.getType() == ConnectivityManager.TYPE_MOBILE;
@@ -60,17 +59,26 @@ public class MainActivity extends AppCompatActivity {
                 wifiConnected = false;
                 mobileConnected = false;
             }
+            String base = getString(R.string.base);
+            if(networkInfo != null) {
+                result = String.format(base, networkInfo.getType(), networkInfo.isAvailable(), networkInfo.isConnected(), networkInfo.isRoaming());
+            }else{
+                result = getString(R.string.no);
+                result += String.format(base, -1, "-", "-", "-");
+            }
             if (wifiConnected) {
                 return getString(R.string.wifi);
-            } else if(mobileConnected) {
+            } else if (mobileConnected) {
                 return getString(R.string.mobile);
             } else {
                 return getString(R.string.noNetwork);
             }
         }
+
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            textView1.setText(result);
             textView2.setText(s);
         }
     }
@@ -79,5 +87,13 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         checkConnectivity();
+    }
+
+    public class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            checkConnectivity();
+        }
     }
 }
